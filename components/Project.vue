531d7884476a0f723eee08667aa58d43413a4c1c
ㅤㅤ <hi@aexhell.life>
2023-04-26 04:28:27 +0200

    feat(Project): background

--- components/Project.vue
+++ components/Project.vue
@@ -1,10 +1,10 @@
 <template>
-   <nuxt-link v-if="link" to="#" class="border hover:shadow-md no-underline border-gray/50 hover:bg-white-100/25 dark:border-white-100/10 transition hover:border-gray/90 dark:hover:border-white-100/25 dark:hover:bg-gray/20 mb-3 flex justify-between items-center rounded-md py-4 px-6">
+   <nuxt-link v-if="link" to="#" class="__AXHL-PROJECT overflow-hidden relative border hover:shadow-md no-underline border-gray/50 hover:bg-white-100/25 dark:border-white-100/15 transition hover:border-gray/90 dark:hover:border-white-100/25 dark:hover:bg-gray/20 mb-3 flex justify-between items-center rounded-md py-4 px-6">
       <h2 class="text-gray dark:text-white-100 font-semibold w-full" v-text="title" />
       <div class="sm:xl:block hidden border-t dark:border-gray border-gray/50 w-full" />
       <p class="text-gray dark:text-white-100 w-full text-right text-sm" v-text="position" />
    </nuxt-link>
-   <div v-else class="border hover:shadow-md no-underline border-gray/50 dark:border-white-100/10 transition hover:border-gray/90 dark:hover:border-white-100/75 dark:hover:bg-gray/20 mb-3 flex justify-between items-center rounded-md py-4 px-6">
+   <div v-else class="__AXHL-PROJECT overflow-hidden relative border hover:shadow-md no-underline border-gray/50 dark:border-white-100/10 transition hover:border-gray/90 dark:hover:border-white-100/75 dark:hover:bg-gray/20 mb-3 flex justify-between items-center rounded-md py-4 px-6">
       <h2 class="text-gray dark:text-white-100 font-semibold w-full" v-text="title" />
       <p class="text-gray dark:text-white-100 w-full text-right text-sm" v-text="position" />
    </div>
