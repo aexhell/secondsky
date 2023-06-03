46993d112acf04370e53327874a7f8ce8751f49f
ㅤㅤ <hi@aexhell.life>
2023-06-03 07:08:25 +0200

    feat(works): change colors

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -1,16 +1,16 @@
 <template>
-   <div id="__secondsky-content" class="w-3/4 px-8 h-full flex">
+   <div id="__secondsky-content" class="w-3/4 md:px-8 h-full flex">
       <div class="w-full h-full py-12 my-auto flex">
          <div class="w-full h-full">
             <div class="w-full mb-8">
                <h2 class="font-bold xl:text-5xl text-gray dark:text-white-200 text-4xl"><span class="lifeisshort">Projects</span></h2>
             </div>
-            <nuxt-link v-for="proj of projects" :key="proj" :to="`works/${proj.code}`" class="w-full border hover:shadow-md no-underline border-gray/50 hover:border-gray/90 dark:hover:border-white-100/75 dark:hover:bg-gray/20 relative dark:border-white-100/10 transition mb-3 flex justify-between items-center rounded-md p-16">
+            <nuxt-link v-for="proj of projects" :key="proj" :to="`works/${proj.code}`" class="w-full border hover:shadow-md no-underline border-gray/50 hover:border-gray/90 dark:hover:border-white-100/75 dark:hover:bg-gray/20 relative dark:border-white-100/10 transition mb-3 flex justify-between items-center rounded-md md:p-16 py-8 px-4">
                <div class="w-full flex justify-between items-center">
-                  <h2 class="font-bold xl:text-3xl text-gray dark:text-white-200 z-10 text-xl capitalize" v-text="proj.code" />
-                  <p class="font-bold xl:text-2xl text-gray dark:text-white-200 z-10 text-lg" v-text="proj.description" />
+                  <h2 class="font-bold drop-shadow-xl md:text-3xl text-white-200 z-10 text-xl capitalize" v-text="proj.code" />
+                  <p class="font-bold xl:text-2xl text-white-200 z-10 text-lg" v-text="proj.description" />
                </div>
-               <div class="__secondsky-project w-full h-full top-0 left-0 absolute" :style="`background: url(/projects/${proj.code}.png)`"></div>
+               <div class="__secondsky-project w-full h-full top-0 rounded-md bg-clip-border bg-no-repeat bg-cover left-0 absolute" :style="`background: url(/projects/${proj.code}.png)`"></div>
             </nuxt-link>
          </div>
       </div>
@@ -23,11 +23,11 @@ import {ArrowRightIcon} from '@heroicons/vue/24/outline';
 const projects = [
    {
       code: 'mylium',
-      description: 'Social media attempt.'
+      description: 'Senior Dev'
    },
    {
       code: 'puroto',
-      description: 'Furry social media.'
+      description: 'Front-end'
    }
 ]
 </script>
